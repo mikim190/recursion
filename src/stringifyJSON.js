66cab975bd383ebec3d144cedb0b95d0a4001406
Kim T <kim200@Kims-MacBook-Pro.local>
2019-04-10 21:57:11 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,65 +3,54 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function (obj) {
-
-  if (typeof obj === "string") {
+var stringifyJSON = function(obj) {
+  if (typeof obj === 'string') {
     return `"${obj}"`;
-    // var splitString = obj.split('');
-    // splitString.unshift('"')
-    // splitString.push('"')
-    // return splitString.join('');
   }
-  if (obj === null) {
+  if (typeof obj === 'function' || obj === undefined)  {
+    return '{}';
+  }
+  if ((typeof obj === 'number' && isNaN(obj)) || (obj === Infinity || undefined)) {
     return 'null';
   }
-  if (typeof obj === 'number' || typeof obj === "boolean") {
+  if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
     return '' + obj;
-  }
-  if (typeof obj === 'function' || obj === undefined) {
-    return '{}';
   }
   if (obj.length === 0) {
     return '[]';
   }
-
-
-  var newArr = [];
+     
+    //input is array
   if (Array.isArray(obj)) {
+    var newArr = [];
     for (var i = 0; i < obj.length; i++) {
-      if (typeof obj[i] === 'number' || typeof obj[i] === 'boolean') {
+      if (typeof obj[i] === 'number' || typeof obj[i] ==='boolean') {
         newArr.push(obj[i]);
       } else {
-        newArr.push(stringifyJSON(obj[i]));
-      }
+        newArr.push(stringifyJSON(obj[i])) //check each ele and return value   
+      }           
     }
     return `[${newArr}]`;
-
   }
-
-  // if (typeof obj === 'object') {
-  //   if (Object.keys(obj).length === 0) {
-  //     return '{}';
-  //   }
-
-  //   var objResult = [];
-  //   for (var key in obj) {  //{a: {b:c}}
-  //     var newKey = `"${key}"`;
-  //     var value = obj[key];
-
-  //     if (typeof value === 'number' || typeof value === 'boolean') {
-  //       objResult.push(newKey + ':' + value);
-  //     }
-  //     if (typeof value === 'function' || value === 'undefined') {
-  //       delete value;
-  //     }
-  //     objResult.push(newKey + ':' + stringifyJSON(value));
-
-  //   }
-
-  //   objResult = objResult.join(',');
-
-  //   return `{${objResult}}`;
-  // }
+    
+   //input is obj
+  var key;
+  var value;
+  if (typeof obj === 'object') {  
+  var newObj = [];   
+    for (var k in obj) { 
+      if (typeof obj[k] === 'function' || obj[k] === undefined) {
+        delete obj[k];
+      }
+      if ((!Object.keys(obj).length) ) {
+        return '{}';
+      }           
+      key = `"${k}":`;
+      value = `${stringifyJSON(obj[k])}` ;
+      newObj.push(key + value);
+    }  
+    return `{${newObj}}`;    
+  }            
+};
 
 
